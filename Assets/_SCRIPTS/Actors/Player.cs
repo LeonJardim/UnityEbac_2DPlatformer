6c5061bebd3ad047aee8d98912ac73713e04dbaf
Leonardo Jardim <vincibleLeon@gmail.com>
2026-02-12 17:27:04 -0300

    setup animations

--- Assets/_SCRIPTS/Actors/Player.cs
+++ Assets/_SCRIPTS/Actors/Player.cs
@@ -8,8 +8,9 @@ public class Player : MonoBehaviour
 {
     #region Variables
     [Header("References")]
-    [SerializeField] private SpriteRenderer sprite;
+    [SerializeField] private Transform sprite;
     private Rigidbody2D rb;
+    private Animator animator;
     private PlayerInputActions playerControls;
     private InputAction move;
     private InputAction jump;
@@ -23,12 +24,19 @@ public class Player : MonoBehaviour
     private bool _isSprinting = false;
     private bool _lastFloorCheck = false;
 
-    [Header("Animation Setup")]
-    [SerializeField] private float jumpScaleY = 1.8f;
-    [SerializeField] private float jumpScaleX = 0.9f;
+    [Header("Squat/Strech Setup")]
+    [SerializeField] private float jumpScaleY = 1.6f;
+    [SerializeField] private float jumpScaleX = 0.7f;
     [SerializeField] private float animationDuration = 0.15f;
     [SerializeField] private Ease ease = Ease.OutBack;
     private Vector3 _initialScale;
+
+    [Header("Animations")]
+    [SerializeField] private float turningDuration = 0.2f;
+    [SerializeField] private string animRunBool = "Run";
+    [SerializeField] private string animYSpeed = "YSpeed";
+    [SerializeField] private string animOnFloorBool = "OnFloor";
+    private bool _isTurning = false;
 
     [Header("Box Cast")]
     [SerializeField] private float castDistance;
@@ -42,7 +50,8 @@ public class Player : MonoBehaviour
     {
         playerControls = new PlayerInputActions();
         rb = GetComponent<Rigidbody2D>();
-        _initialScale = sprite.transform.localScale;
+        animator = sprite.GetComponent<Animator>();
+        _initialScale = sprite.localScale;
     }
     private void OnEnable()
     {
@@ -67,17 +76,32 @@ public class Player : MonoBehaviour
     
 
     private void HandleMovement() 
-    {
-        _isSprinting = sprint.IsPressed();
+    {    // Movement
         Vector2 inputDirection = move.ReadValue<Vector2>();
-        float targetSpeed = inputDirection.x;
-        targetSpeed *= _isSprinting ? sprintSpeed : speed;
+        float targetSpeed = inputDirection.x * GetSpeed;
 
         rb.linearVelocityX = Mathf.MoveTowards(rb.linearVelocityX, targetSpeed, friction * Time.deltaTime);
+
+         // Set Animations
+        if (Mathf.Abs(rb.linearVelocityX) > 0.1f)
+        {
+            animator.SetBool(animRunBool, true);
+            animator.speed = _isSprinting ? 2f : 1f;
+            FlipSprite();
+        }
+        else
+        {
+            animator.SetBool(animRunBool, false);
+            animator.speed = 1f;
+        }
+        
     }
     private void HandleJump() 
     {
         bool isOnFloor = IsOnFloor();
+
+        if (Mathf.Abs(rb.linearVelocityY) > 0) { animator.SetFloat(animYSpeed, rb.linearVelocityY > 0f ? 1f : -1f); }
+        else { animator.SetFloat(animYSpeed, 0f); }
 
         if (isOnFloor && jump.triggered)
         {
@@ -94,8 +118,8 @@ public class Player : MonoBehaviour
     }
     private void JumpAnimation(bool landing = false)
     {
-        sprite.transform.localScale = _initialScale;
-        DOTween.Kill(sprite.transform);
+        sprite.localScale = _initialScale;
+        DOTween.Kill(sprite);
         float sX = jumpScaleX;
         float sY = jumpScaleY;
         float d = animationDuration;
@@ -105,21 +129,45 @@ public class Player : MonoBehaviour
             sY = 1.0f / sY;
             d *= 0.5f;
         }
-        sprite.transform.DOScaleY(sprite.transform.localScale.y * sY, d).SetLoops(2, LoopType.Yoyo).SetEase(ease);
-        sprite.transform.DOScaleX(sprite.transform.localScale.x * sX, d).SetLoops(2, LoopType.Yoyo).SetEase(ease);
+        sprite.DOScaleY(sprite.localScale.y * sY, d).SetLoops(2, LoopType.Yoyo).SetEase(ease);
+        sprite.DOScaleX(sprite.localScale.x * sX, d).SetLoops(2, LoopType.Yoyo).SetEase(ease);
+    }
+    private void FlipSprite()
+    {
+        if (transform.localScale.x * rb.linearVelocityX < 0 && !_isTurning)
+        {
+            _isTurning = true;
+            transform.DOScaleX(-transform.localScale.x, turningDuration)
+                .OnComplete(() => _isTurning = false);
+        }
     }
 
+
+    #region Variable Functions
     private bool IsOnFloor()
     {
         if (Physics2D.BoxCast(transform.position, castBoxSize, 0f, Vector2.down, castDistance, groundLayer))
-            { return true; }
+            {  animator.SetBool(animOnFloorBool, true); return true;}
         else
-            { return false; }
+            { animator.SetBool(animOnFloorBool, false); return false; }
     }
-    /*
-    private void OnDrawGizmos()
+    //private void OnDrawGizmos() { Gizmos.DrawWireCube(transform.position + (Vector3.down * castDistance), castBoxSize); }
+    public float GetSpeed
     {
-        Gizmos.DrawWireCube(transform.position + Vector3.down * castDistance, castBoxSize);
+        get
+        {
+            if (sprint.IsPressed())
+            {
+                _isSprinting = true;
+                return sprintSpeed;
+            }
+            else
+            {
+                _isSprinting = false;
+                return speed;
+            }
+        }
+        set { speed = value; }
     }
-    */
+    #endregion
 }
