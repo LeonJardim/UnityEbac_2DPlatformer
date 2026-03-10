2bd583064af1d8142875a21547df9b58301ba618
Leonardo Jardim <vincibleLeon@gmail.com>
2026-03-09 23:42:09 -0300

    remove unecessary code

--- Assets/_SCRIPTS/Managers/ItemManager.cs
+++ Assets/_SCRIPTS/Managers/ItemManager.cs
@@ -5,7 +5,7 @@ using UnityEngine;
 public class ItemManager : Singleton<ItemManager>
 {
     public SOInt coins;
-    [SerializeField] private TMP_Text coinTextHUD;
+    //[SerializeField] private TMP_Text coinTextHUD;
 
     private void Start()
     {
